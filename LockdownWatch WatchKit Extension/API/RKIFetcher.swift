d0b19e172a20c627eccb37218dbafa8b6ebb20ca
SplittyDev <splittydev@gmail.com>
2021-04-18 13:40:08 +0200

    Add incidence and vaccination data from RKI

---
+++ LockdownWatch WatchKit Extension/API/RKIFetcher.swift
@@ -0,0 +1,104 @@
+//
+//  RKIFetcher.swift
+//  LockdownWatch WatchKit Extension
+//
+//  Created by Marco Quinten Privat on 18.04.21.
+//
+
+import Foundation
+import SwiftUI
+
+let URL_RKI_GERMANY = URL(string: "https://api.corona-zahlen.org/germany")!
+let URL_RKI_VACCINATIONS = URL(string: "https://api.corona-zahlen.org/vaccinations")!
+
+struct RKIDataGermany: Codable {
+    struct Delta: Codable {
+        var cases: Int
+        var deaths: Int
+        var recovered: Int
+    }
+    
+    struct R: Codable {
+        var value: Double
+        var date: String
+    }
+    
+    let cases: Int
+    let deaths: Int
+    let recovered: Int
+    let weekIncidence: Double
+    let casesPer100k: Double
+    let casesPerWeek: Double
+    let delta: Delta
+    let r: R
+}
+
+struct RKIVaxData: Codable {
+    struct Data: Codable {
+        struct SecondVaccination: Codable {
+            let delta: Int
+            let quote: Double
+        }
+        
+        let delta: Int
+        let quote: Double
+        let secondVaccination: SecondVaccination
+    }
+    
+    let data: Data
+}
+
+class RKIFetcher: ObservableObject {
+    private let jsonDecoder = JSONDecoder()
+    
+    @Published var dataGermany: RKIDataGermany?
+    @Published var vaxData: RKIVaxData?
+    
+    init() {
+        dataGermany = nil
+        vaxData = nil
+    }
+    
+    func fetch() {
+        fetchGermany()
+        fetchVaccinations()
+    }
+    
+    private func fetchGermany() {
+        URLSession.shared.dataTask(with: URL_RKI_GERMANY) { data, resp, err in
+            
+            // Validate response
+            guard err == nil else { return }
+            guard let data = data else { return }
+            guard (resp as? HTTPURLResponse)?.statusCode == 200 else { return }
+            
+            // Deserialize JSON
+            if let data = try? self.jsonDecoder.decode(RKIDataGermany.self, from: data) {
+                DispatchQueue.main.async {
+                    self.dataGermany = data
+                }
+            } else {
+                print("FAILED TO DECODE DATA")
+            }
+        }.resume()
+    }
+    
+    private func fetchVaccinations() {
+        URLSession.shared.dataTask(with: URL_RKI_VACCINATIONS) { data, resp, err in
+            
+            // Validate response
+            guard err == nil else { return }
+            guard let data = data else { return }
+            guard (resp as? HTTPURLResponse)?.statusCode == 200 else { return }
+            
+            // Deserialize JSON
+            if let data = try? self.jsonDecoder.decode(RKIVaxData.self, from: data) {
+                DispatchQueue.main.async {
+                    self.vaxData = data
+                }
+            } else {
+                print("FAILED TO DECODE DATA")
+            }
+        }.resume()
+    }
+}
--- LockdownWatch WatchKit Extension/Views/HomeView.swift
+++ LockdownWatch WatchKit Extension/Views/HomeView.swift
@@ -11,11 +11,19 @@ import UserNotifications
 
 struct HomeView: View {
     let config = Config.shared
+    
     @ObservedObject var model = HomeViewModel()
     @ObservedObject var state: AppState = .shared
+    @ObservedObject var rkiFetcher = RKIFetcher()
+    
+    init() {
+        rkiFetcher.fetch()
+    }
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center, spacing: 0) {
+            Spacer()
+            
             Gauge(
                 value: model.currentHour,
                 in: 0...24,
@@ -29,7 +37,34 @@ struct HomeView: View {
             )
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .scaleEffect(2.0)
+            
             Text(model.lockdownState.text)
+            
+            Spacer()
+            
+            VStack(alignment: .center, spacing: 2) {
+                if let data = rkiFetcher.dataGermany {
+                    HStack(alignment: .center) {
+                        Text("📈")
+                        Text("IW \(Int(data.weekIncidence))")
+                        Text("R \(data.r.value, specifier: "%.2f")")
+                    }.frame(maxWidth: .infinity, maxHeight: 20)
+                }
+                
+                if let data = rkiFetcher.vaxData {
+                    HStack(alignment: .center) {
+                        Text("💉")
+                        Text("\(data.data.quote * 100, specifier: "%.2f")% first vax")
+                    }.frame(maxWidth: .infinity, maxHeight: 20)
+                }
+            }.font(.footnote).frame(maxWidth: .infinity)
+            
+            Spacer()
+        }
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 60, repeats: true, block: { _ in
+                self.rkiFetcher.fetch()
+            })
         }
     }
 }
